55b705cb25ecfe63f0ef2971171d3b04e92eb55e
Youssef Ahmed Ramdan <149728139+Youssef-Ahmed38@users.noreply.github.com>
2025-07-06 20:41:07 +0200

    ذاكر ياعم البوت

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
-from flask import Flask, request, jsonify
-from app.gpt_service import generate_response_with_context
+from flask import Flask, request, jsonify 
+from App.gpt_service import generate_response_with_rag_context_ROADMAP, generate_response_with_rag_context_General 
 
 app = Flask(__name__)
 
-@app.route("/generate", methods=["POST"])
-def generate_response():
+@app.route("/generate_roadmap", methods=["POST"])
+def generate_response_roadmap():
     prompt = request.form.get("prompt")
     session_id = request.form.get("session_id", None, type=int)
     username = request.form.get("username", "default_user")
@@ -13,7 +13,25 @@ def generate_response():
     if not prompt:
         return jsonify({"error": "Prompt is required"}), 400
 
-    response = generate_response_with_context(
+    response = generate_response_with_rag_context_ROADMAP(
+        user_input=prompt,
+        session_id=session_id,
+        username=username,
+        email=email
+    )
+    return jsonify({"response": response})
+
+@app.route("/generate", methods=["POST"])
+def generate_response_general():
+    prompt = request.form.get("prompt")
+    session_id = request.form.get("session_id", None, type=int)
+    username = request.form.get("username", "default_user")
+    email = request.form.get("email", "default@example.com")
+
+    if not prompt:
+        return jsonify({"error": "Prompt is required"}), 400
+
+    response = generate_response_with_rag_context_General(
         user_input=prompt,
         session_id=session_id,
         username=username,
@@ -22,4 +40,4 @@ def generate_response():
     return jsonify({"response": response})
 
 if __name__ == "__main__":
-    app.run(host="127.0.0.1", port=8000, debug=True)
+    app.run(host="127.0.0.1", port=8000, debug=False)
