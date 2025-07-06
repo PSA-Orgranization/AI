735e1cf28c3e1b0c5d316986dc3d99a02550b9f9
Youssef Ahmed Ramdan <149728139+Youssef-Ahmed38@users.noreply.github.com>
2025-07-06 20:48:40 +0200

    Delete app/db_conn.py

--- app/db_conn.py
+++
@@ -1,8 +0,0 @@
-import sqlite3  # Replaces pyodbc
-
-# Connect to SQLite database (creates if doesn't exist)
-conn = sqlite3.connect('psa.sqlite' , check_same_thread=False)  # Database stored in psa.sqlite file
-cursor = conn.cursor()
-
-def get_db_cursor():
-    return conn.cursor()
